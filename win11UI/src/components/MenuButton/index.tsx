ea8d82c8e82194d2fc83e158dfb8d623fcbfb61b
Maykon Felix <109165918+MaykonFelix@users.noreply.github.com>
2023-02-04 01:40:55 -0300

    Update

---
+++ win11UI/src/components/MenuButton/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+const MenuButton = () = {
+    return(
+        <Button 
+        sx = {{
+            height: 40,
+            width: 40,
+            '&:hover': {
+                backgroundColor: "rgba(255.255.255.0.01",
+            } }} >
+        </Button >
+    )
+
+}
+
+export default MenuButton
--- win11UI/src/containers/MenuBar/index.tsx
+++ win11UI/src/containers/MenuBar/index.tsx
@@ -1,9 +1,25 @@
+import { Box } from "@mui/material";
+import { Stack } from "@mui/system";
 import React from "react";
 
 const MenuBar = () => {
-return (
-    <div>MenuBar</div>
-)
+    return (
+        <Box
+            sx={(theme) => ({
+                width: "100vw",
+                height: 50,
+                backgroundColor: `${theme.palette.background.grey}D9`,
+                backdropFilter: "blur(2px)",
+            })}
+        >
+            <Stack
+                direction='row'
+                aligntItems="center"
+                spacing={0.5}
+                sx={{ height: "100%", pl: 1.8 }}
+            ></Stack>
+        </Box>
+    )
 }
 
 export default MenuBar
