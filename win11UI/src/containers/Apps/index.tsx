f5a73d37e9587faeddd011af05de9295c3e7d62a
Maykon Felix <109165918+MaykonFelix@users.noreply.github.com>
2023-02-06 13:35:11 -0300

    Update

--- win11UI/src/containers/Apps/index.tsx
+++ win11UI/src/containers/Apps/index.tsx
@@ -1,9 +1,33 @@
+import { Box } from "@mui/material"
+import React, { useEffect, useState } from "react"
+import { desktopApps } from "../../utils/apps";
 
 
 const Apps = () => {
-return (
-    <div>Apps</div>
-)
+    const [app, setApps] = useState({});
+
+    useEffect(() => {
+        const _apps = {};
+
+        desktopApps.map((app, i) => {
+            _apps[app.name] = {
+              left: 20,
+              top: i * 100 + 20,
+              name: app.name,
+              icon: app.icon,
+            };
+          });
+      
+          setApps(_apps);
+        }, []);
+
+    return (
+        < Box sx={{ width: '100%', height: '100%', position: 'relative' }
+        }>
+
+
+        </Box >
+    )
 }
 
 export default Apps
